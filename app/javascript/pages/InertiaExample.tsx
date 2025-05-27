6088f37f34b85ece644bbda6084b51ad313af7cb
Ela Kozera <kozerka@gmail.com>
2025-05-27 13:28:38 +0200

    chore: add shadcn/ui (#1)

--- app/javascript/pages/InertiaExample.tsx
+++ app/javascript/pages/InertiaExample.tsx
@@ -1,60 +1,69 @@
-import { Head } from '@inertiajs/react'
-import { useState } from 'react'
+import { Head } from "@inertiajs/react";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import inertiaSvg from "/assets/inertia.svg";
+import reactSvg from "/assets/react.svg";
+import viteRubySvg from "/assets/vite_ruby.svg";
 
-import inertiaSvg from '/assets/inertia.svg'
-import reactSvg from '/assets/react.svg'
-import viteRubySvg from '/assets/vite_ruby.svg'
-
-import cs from './InertiaExample.module.css'
+import cs from "./InertiaExample.module.css";
 
 export default function InertiaExample({ name }: { name: string }) {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <Head title="Inertia + Vite Ruby + React Example" />
+    return (
+        <>
+            <Head title="Inertia + Vite Ruby + React Example" />
 
-      <div className={cs.root}>
-        <h1 className={cs.h1}>Hello {name}!</h1>
+            <div className={cs.root}>
+                <h1 className={cs.h1}>Hello {name}!</h1>
+                <Button>Button</Button>
+                <p className="text-red-500 bg-pink-300">Hello</p>
+                <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 h-9 px-4 py-2">
+                    Test Button with direct classes
+                </button>
+                <div>
+                    <a href="https://inertia-rails.dev" target="_blank">
+                        <img
+                            className={cs.logo}
+                            src={inertiaSvg}
+                            alt="Inertia logo"
+                        />
+                    </a>
+                    <a href="https://vite-ruby.netlify.app" target="_blank">
+                        <img
+                            className={`${cs.logo} ${cs.vite}`}
+                            src={viteRubySvg}
+                            alt="Vite Ruby logo"
+                        />
+                    </a>
+                    <a href="https://react.dev" target="_blank">
+                        <img
+                            className={`${cs.logo} ${cs.react}`}
+                            src={reactSvg}
+                            alt="React logo"
+                        />
+                    </a>
+                </div>
 
-        <div>
-          <a href="https://inertia-rails.dev" target="_blank">
-            <img className={cs.logo} src={inertiaSvg} alt="Inertia logo" />
-          </a>
-          <a href="https://vite-ruby.netlify.app" target="_blank">
-            <img
-              className={`${cs.logo} ${cs.vite}`}
-              src={viteRubySvg}
-              alt="Vite Ruby logo"
-            />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img
-              className={`${cs.logo} ${cs.react}`}
-              src={reactSvg}
-              alt="React logo"
-            />
-          </a>
-        </div>
+                <h2 className={cs.h2}>Inertia + Vite Ruby + React</h2>
 
-        <h2 className={cs.h2}>Inertia + Vite Ruby + React</h2>
-
-        <div className="card">
-          <button
-            className={cs.button}
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is {count}
-          </button>
-          <p>
-            Edit <code>app/frontend/pages/InertiaExample.jsx</code> and save to
-            test HMR
-          </p>
-        </div>
-        <p className={cs.readTheDocs}>
-          Click on the Inertia, Vite Ruby, and React logos to learn more
-        </p>
-      </div>
-    </>
-  )
+                <div className="card">
+                    <button
+                        className={cs.button}
+                        onClick={() => setCount((count) => count + 1)}
+                    >
+                        count is {count}
+                    </button>
+                    <p>
+                        Edit <code>app/frontend/pages/InertiaExample.jsx</code>{" "}
+                        and save to test HMR
+                    </p>
+                </div>
+                <p className={cs.readTheDocs}>
+                    Click on the Inertia, Vite Ruby, and React logos to learn
+                    more
+                </p>
+            </div>
+        </>
+    );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import RubyPlugin from "vite-plugin-ruby";
+import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    RubyPlugin(),
-  ],
-})
+    plugins: [react(), tailwindcss(), RubyPlugin()],
+    resolve: {
+        alias: {
+            "@": path.resolve(process.cwd(), "app/frontend"),
+        },
+    },
+});
